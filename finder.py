3d44463b8c598511962d6685907091b80fb465fe
tony <tonychau117@gmail.com>
2025-04-04 22:03:05 -0700

    slight change in order

--- finder.py
+++ finder.py
@@ -26,12 +26,6 @@ from selenium.webdriver.common.by import By #
 # google maps url:  https://www.google.com/maps/@32.7678627,-117.0676283,3643m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MDMxOC4wIKXMDSoJLDEwMjExNjQwSAFQAw%3D%3D
 url = 'https://www.google.com/maps/@32.7678627,-117.0676283,3643m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MDMxOC4wIKXMDSoJLDEwMjExNjQwSAFQAw%3D%3D'
 
-driver = webdriver.Chrome() # browser to use
-driver.get(url)
-
-
-driver.implicitly_wait(10) # using an implicit wait to help us sync browser and code so that it does not return an exception
-
 # use a while loop to ask for the kind of input (zip code vs city)
 # prototype implementation
 
@@ -39,31 +33,27 @@ type = ''
 while type != 'CITY' or type != 'ZIP CODE':
     type = input("Enter if you are searching by CITY or ZIP CODE")
 
-    if type.upper() == 'CITY' or type == type.upper() == 'ZIP CODE': # if it equals to city, use the city as the input for our method
-        break
-    else: # else if it doesnt match the upper if statement we will continue to loop the code until we get what we need
-        continue
+if type == 'CITY': # if it equals to city, use the city as the input for our method
+    pass
+else: # else if not city, we will pass in the zipcode
+    pass
 
 # prompt the user for what they are searching for in the area
 reason = ''
-while reason == '': 
+while reason == '':
     reason = input(f'What are you searching for in {type}')
-    # pass in the reason, dont make a bunch of unneeded if statements
-textbox = driver.find_element(By.CLASS_NAME, 'fontBodyMedium searchboxinput xiQnY ')
 
-textbox.clear()
-textbox.send_keys(reason)
-textbox.send_keys(Keys.ENTER)
+driver = webdriver.Chrome() # browser to use
+driver.get(url)
 
-'''
-<input class="fontBodyMedium searchboxinput xiQnY " role="combobox" jslog="11886" aria-controls="ydp1wd-haAclf" 
-aria-expanded="false" aria-haspopup="grid" autocomplete="off" autofocus="" id="searchboxinput" name="q" jsaction="keyup:omnibox.keyUp; 
-input:omnibox.inputDetected; keydown:omnibox.keyDown; focus:omnibox.focus; blur:omnibox.blur">
-'''
+driver.implicitly_wait(5) # using an implicit wait to help us sync browser and code so that it does not return an exception
 
 
 
 # temp implementation of passing in CITY or ZIP CODE
 # ex. VARIABLE.send_keys('REASON in CITY/ZIP CODE')
+
+# create a list of locations            
+# filter by class name or class ID
 
 driver.close() # closes the browser
