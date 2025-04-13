0a5e2406e20662ed341a16fbda93b3bc1533c345
tony <tonychau117@gmail.com>
2025-04-13 03:02:10 -0700

    create a class object to hold info

---
+++ location.py
@@ -0,0 +1,15 @@
+class Location:
+    def __init__(self, name, rating, cost):
+        self.name = name
+        self.rating = rating
+        self.cost = cost
+    
+    def getName(self):
+        return self.name
+    
+    def getRating(self):
+        return self.rating
+    
+    def getCost(self):
+        return self.cost
+
